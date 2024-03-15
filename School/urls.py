723ec2d2b874fe89f638177ae838190e4d8e5b6e
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-03-15 17:32:23 -0500

    correccion

--- School/urls.py
+++ School/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pagina import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pagina.urls'))
+    path('', views.index, name="index"),
 ]
