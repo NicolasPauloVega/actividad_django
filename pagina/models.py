e5f03e6a3e931aaa404c4f73ff312088ab1e4bf4
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-03-16 13:27:27 -0500

    b1

--- pagina/models.py
+++ pagina/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Materi(models.Model):
+    code = models.AutoField(primary_key=True, default=1, verbose_name="codigo")
+    name = models.CharField(max_length=150, verbose_name="nombre")
+    description = models.TextField(verbose_name="descripcion")
+    credits = models.IntegerField(verbose_name="creditos")
+    Creation_date = models.DateTimeField(auto_now_add=True, verbose_name="fecha de creación")
+    update_date = models.DateField(auto_now=True, verbose_name="fecha de actualización")
+    class Meta:
+        #db_table=""
+        verbose_name="Materia"
+        verbose_name="Materias"
