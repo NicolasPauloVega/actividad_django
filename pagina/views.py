45dc4c95b06d2f9b62fcb76661cbda472f4efcd1
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-03-15 21:14:36 -0500

    a7

--- pagina/views.py
+++ pagina/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'layout.html')
+    return render(request, 'index.html', {
+        'mi_variable': 'soy un dato que esta en la vista',
+        'title': 'Inicio del sitio',
+        'titulo': 'Página de Inicio',
+    })
