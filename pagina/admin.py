6cddcdc46a186ddf19a0354e7f0bdea282872c9e
williamstiwarperez <128011305+williamstiwarperez@users.noreply.github.com>
2024-03-21 06:58:50 -0500

    d8
    St3

--- pagina/admin.py
+++ pagina/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Materi, Careers, students, Teacher
 
 # Register your models here.
-admin.site.register(Materi)
+class MateriAdmin(admin.ModelAdmin):
+    readonly_fields=('Creation_date', 'update_date')
+admin.site.register(Materi, MateriAdmin)
 admin.site.register(Careers)
 admin.site.register(students)
 admin.site.register(Teacher)
