4f2872719540c127e492c4756456826e32ea8574
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-04 07:00:06 -0500

    e3

--- pagina/views.py
+++ pagina/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 # Vista para la página de inicio
 def index(request):
     # Retorna la plantilla 'index.html' renderizada junto con algunos datos adicionales
-    return render(request, 'index.html', {
+    return render(request, 'nav/index.html', {
         'my_variable': 'soy un dato que esta en la vista',
         'title': 'Inicio del sitio',
         'titulo': 'Página de Inicio',
@@ -17,29 +17,29 @@ def index(request):
 # Vista para la página futura (future)
 def future(request):
     # Retorna la plantilla 'future.html' renderizada
-    return render(request, 'future.html')
+    return render(request, 'nav/future.html')
 
 # Vista para los programas académicos
 def careers(request):
     career = Careers.objects.order_by('code')
     # Retorna la plantilla 'careers.html' renderizada
-    return render(request, 'careers.html',{
+    return render(request, 'nav/careers.html',{
         'career': career
     })
 
 def courses(request):
     materi = Materi.objects.order_by('code')
-    return render(request, 'courses.html',{
+    return render(request, 'nav/courses.html',{
         'materi': materi
     })
 
 def student(request):
     students_list = students.objects.all()
-    return render(request, 'students.html', {'students': students_list})
+    return render(request, 'nav/students.html', {'students': students_list})
 
 def teachers(request):
     teacher = Teacher.objects.order_by('code')
-    return render(request, 'teachers.html', {
+    return render(request, 'nav/teachers.html', {
         'teacher': teacher
     })
 
@@ -52,7 +52,7 @@ def create_materi(request):
             return redirect('courses')  # Redirect to courses after successful form submission
     else:
         form = MateriForm()
-    return render(request, 'create_materi.html', {'form': form})
+    return render(request, 'forms/create_materi.html', {'form': form})
 
 def create_career(request):
     if request.method == 'POST':
@@ -63,7 +63,7 @@ def create_career(request):
             return redirect('careers')  # Redirect to careers after successful form submission
     else:
         form = CareersForm()
-    return render(request, 'create_career.html', {'form': form, 'messages': messages.get_messages(request)})
+    return render(request, 'forms/create_career.html', {'form': form, 'messages': messages.get_messages(request)})
 
 def create_student(request):
     if request.method == 'POST':
@@ -74,7 +74,7 @@ def create_student(request):
             return redirect('students')  # Redirect to students after successful form submission
     else:
         form = StudentsForm()
-    return render(request, 'create_student.html', {'form': form})
+    return render(request, 'forms/create_student.html', {'form': form})
 
 def create_teacher(request):
     if request.method == 'POST':
@@ -84,4 +84,4 @@ def create_teacher(request):
             return redirect('teachers')
     else:
         form = TeacherForm()
-    return render(request, 'create_teacher.html', {'form': form})
+    return render(request, 'forms/create_teacher.html', {'form': form})
