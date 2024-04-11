37486e24bae3898d4783f7d742bd58bc00735a5d
NicolasPV <145580278+NicolasPauloVega@users.noreply.github.com>
2024-04-10 20:44:01 -0500

    h1

--- pagina/forms.py
+++ pagina/forms.py
@@ -5,53 +5,62 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class MateriForm(forms.ModelForm):
+    # Define un formulario utilizando el modelo Materi.
     class Meta:
+        # Asocia el formulario con el modelo Materi.
         model = Materi
+        # Define los campos del modelo que estarán presentes en el formulario.
         fields = ['name', 'description', 'credits']
-        #Nombre personalizado para cada campo del formulario
+        # Define nombres personalizados para cada campo del formulario.
         labels = {
             'name': 'Nombre',
-            'description': 'Descripcion',
-            'credits': 'Creditos'
+            'description': 'Descripción',
+            'credits': 'Créditos'
         }
-        #Le asignamos un widget personalizado para cada campo del formulario
+        # Asigna widgets personalizados para cada campo del formulario, que controlan su apariencia y comportamiento.
         widgets = {
-        'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
-        'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción'}),
-        'credits': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Créditos'}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción'}),
+            'credits': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Créditos'}),
         }
 
 class CareersForm(forms.ModelForm):
+    # Define un formulario utilizando el modelo Careers.
     class Meta:
+        # Asocia el formulario con el modelo Careers.
         model = Careers
+        # Define los campos del modelo que estarán presentes en el formulario.
         fields = ['name', 'duration']
-        #Nombre personalizado para cada campo del formulario
+        # Define nombres personalizados para cada campo del formulario.
         labels = {
             'name': 'Nombre',
-            'duration': 'Duracion'
+            'duration': 'Duración'
         }
-        #Le asignamos un widget personalizado para cada campo del formulario
-        widgets= {
+        # Asigna widgets personalizados para cada campo del formulario, que controlan su apariencia y comportamiento.
+        widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
-            'duration': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Duracion'})
+            'duration': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Duración'})
         }
 
 class StudentsForm(forms.ModelForm):
+    # Define un formulario utilizando el modelo students.
     class Meta:
+        # Asocia el formulario con el modelo students.
         model = students
+        # Define los campos del modelo que estarán presentes en el formulario.
         fields = ['name', 'last_name', 'email', 'phone', 'photo', 'date_of_birth']
-        #Nombre personalizado para cada campo del formulario
+        # Define nombres personalizados para cada campo del formulario.
         labels = {
             'name': 'Nombre',
             'last_name': 'Apellido',
-            'email': 'Correo electronico',
-            'phone': 'Telefono',
+            'email': 'Correo electrónico',
+            'phone': 'Teléfono',
             'date_of_birth': 'Fecha de nacimiento'
         }
-        #Le asignamos un widget personalizado para cada campo del formulario
+        # Asigna widgets personalizados para cada campo del formulario, que controlan su apariencia y comportamiento.
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'las_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'phone': forms.TextInput(attrs={'class': 'form-control'}),
             'photo': forms.FileInput(attrs={'class': 'form-control-file'}),
@@ -83,6 +92,9 @@ class TeacherForm(forms.ModelForm):
 
 #formulario para el registro de usario
 class RegisterForm(UserCreationForm):
+    # Define un formulario de registro de usuario que hereda de UserCreationForm.
     class Meta:
+        # Asocia el formulario con el modelo User.
         model = User
+        # Define los campos del modelo que estarán presentes en el formulario.
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
